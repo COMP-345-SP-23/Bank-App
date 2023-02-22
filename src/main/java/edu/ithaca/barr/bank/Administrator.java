7572822e312207ac2de14862935c0ce904011b9a
Vanessa <vmpofu@ithaca.edu>
2023-02-22 11:16:44 -0500

    wrote tests for the administrator class

---
+++ src/main/java/edu/ithaca/barr/bank/Administrator.java
@@ -0,0 +1,26 @@
+package edu.ithaca.barr.bank;
+
+public class Administrator {
+    
+    public double checkOverallBalance(){
+        // TODO Auto-generated method stub
+        return -1.0;
+    }
+
+    public boolean freezeAccount(int accNum){
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean unfreezeAccount(int accNum){
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public void checkSuspiciousNess(int accNum){
+        // TODO Auto-generated method stub
+
+    }
+
+
+}
