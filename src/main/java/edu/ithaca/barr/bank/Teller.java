3289639782dee58d2a3c401923f699999bd502d7
Nardos Mamo <nmamo@ithaca.edu>
2023-02-23 13:44:20 -0500

    Wrote Implementation for teller

--- src/main/java/edu/ithaca/barr/bank/Teller.java
+++ src/main/java/edu/ithaca/barr/bank/Teller.java
@@ -6,32 +6,31 @@ public class Teller implements ATM_teller{
     public CentralBank bank;
    
     public void createAccount(Customer customer, int acc_num){
-       
+        Account acc = new Account(acc_num,0.0,customer);
+        bank.addAccounts(acc); 
     }
 
     public void deleteAccount(Account account){
-    
+        bank.deleteAccount(account);
     }
 
     public double checkBalance(Account account) {
-        return 0;
-        
+        return account.checkBalance();
     }
     
     public void deposit(Account account, double amount){
-        
+        account.deposit(amount);
     }
  
     public ArrayList<Double> seeTransaction(Account account){
-        return null;
-       
+        return account.getTransactionHistory();
     }
        
     public void withdraw(Account account,double amount){
-      
+        account.withdraw(amount);
     }
 
     public void transfer(Account accountSender, Account accountReciever, double amount){
-        
+        accountSender.transfer(accountReciever, amount);
     }
 }
