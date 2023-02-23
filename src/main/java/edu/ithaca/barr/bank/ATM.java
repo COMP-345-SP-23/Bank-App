981499c8154df8725fc05e5ed2d5627b36fc70c5
simre <smelak@ithaca.edu>
2023-02-22 20:03:07 -0500

    Wrote comments on what the methods do

--- src/main/java/edu/ithaca/barr/bank/ATM.java
+++ src/main/java/edu/ithaca/barr/bank/ATM.java
@@ -11,23 +11,30 @@
  
  public class ATM implements ATM_Teller_Interface{
      public CentralBank bank;
- 
+ //@checkBalance returns the balance of the account passed to it
      public double checkBalance(Account account) {
          return account.checkBalance();
      }
-     
+
+
+ //@deposit deposits the amount of money passed in the account that is passed    
      public void deposit(Account account, double amount){
          account.deposit(amount);
      }
-  
+
+
+  //@seeTransaction returns the transaction history of the account passed
      public ArrayList<Double> seeTransaction(Account account){
          return account.getTransactionHistory();
      }
         
+
+  //@withdraw withdraws the amount of money passed from the account passed to it
      public void withdraw(Account account,double amount){
          account.withdraw(amount);
      }
  
+ //@transfer transfers the amount of money passed form one account to another
      public void transfer(Account accountSender, Account accountReciever, double amount){
          accountSender.transfer(accountReciever, amount);
      }
